b4a2a463280d841ded5b34ce4e1127378a4e32f9
avoforme <hophuongthanh028@gmail.com>
2025-09-13 17:00:28 -0400

    why bug???

---
+++ data/words.js
@@ -0,0 +1,16 @@
+const words = [
+    {
+        "vn": "Chào",
+        "eng": "Hi"
+    },
+    {
+        "vn": "Tôi",
+        "eng": "I"
+    },
+    {
+        "vn": "Bạn",
+        "eng": "You"
+    }
+]
+
+export default words;
